09d046bee75d41a34e18c5eb1f0339532be07a7a
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-09-12 16:10:58 +0100

    Functionality to change submenu size dynamically

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,15 +2,28 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen, location, page:{page, links} } = useGlobalContext();
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
 
   const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
   useEffect(() => {
+    setColumns('col-2')
     const submenu = container.current;
-    const {center, bottom} = location;
-    submenu.style.left = `${center}px`
-    submenu.style.top = `${bottom}px`
-  }, [location]);
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if(links.length === 3) {
+      setColumns('col-3')
+    }
+    if(links.length > 3) {
+      setColumns('col-4')
+    }
+  }, [location, links]);
 
   return (
     <aside
@@ -18,7 +31,17 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}></div>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url} key={index}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
     </aside>
   );
 };
